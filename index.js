1d5e90c41b8d08ff71477573d9882d9ca3333f62
Andrew Harper <andrewjharper01@gmail.com>
2020-01-06 20:28:14 -0600

    :sparkles: renders home page spa

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 // 'Destructure' each of the named components from the components directory.
 import { Header, Nav, Main, Footer } from "./components";
 
-console.log(Header, Nav, Main, Footer);
+document.querySelector(
+  "#root"
+).innerHTML = `${Header} ${Nav} ${Main} ${Footer}`;
