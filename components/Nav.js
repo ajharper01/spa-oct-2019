636ad59ba5c5d3f72237e94a89833910032e73d6
Andrew Harper <andrewjharper01@gmail.com>
2020-01-07 20:02:00 -0600

    refactor nav to use state

--- components/Nav.js
+++ components/Nav.js
@@ -1,17 +1,11 @@
-function createListHTML(links){
-  return links.map(link=> (<li>${link}</li>))
+function createListHTML(links) {
+  return links
+    .map(link => `<li class="button"><a href=#">${link}</a></li>`)
+    .join("");
 }
 
-export default () => { (parameter) st: any
-  console.log("nav got st", st);
-  return `<nav>
+export default st => `<nav>
   <ul class="flex justify-space-around">
-    ${createListHTML(links)}
-    <li class="button"><a href="#">Home</a></li class="button">
-    <li class="button"><a href="./Contact/">Contact</a></li class="button">
-    <li class="button"><a href="./Resources/">Resources</a></li class="button">
-    <li class="button"><a href="./Keep/">Keep</a></li class="button">
-    <li class="button"><a href="./Method/">Method</a></li>
+    ${createListHTML(st)}
   </ul>
 </nav>`;
-};
